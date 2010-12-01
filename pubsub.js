1bae8aa999ea0e0c29c5002166de9ba9f036c42a
Nate Morse <morse.nate@gmail.com>
2010-12-01 10:57:20 -0800

    I wanted a consistent function argument signature. One argument (or none at all) for a coding style that packages up name/value pairs in one object, not an array of unnamed args.

--- pubsub.js
+++ pubsub.js
@@ -14,22 +14,23 @@
 	// the topic/subscription hash
 	var cache = {};
 
-	d.publish = function(/* String */topic, /* Array? */args){
+	d.publish = function(/* String */topic, /* Any */args){
 		// summary: 
 		//		Publish some data on a named topic.
 		// topic: String
 		//		The channel to publish on
-		// args: Array?
-		//		The data to publish. Each array item is converted into an ordered
-		//		arguments on the subscribed functions. 
+		// args: Any
+		//		The data to publish. The single
+		//		argument (may be a hash of arguments) passed to the subscribed functions. 
 		//
 		// example:
 		//		Publish stuff on '/some/topic'. Anything subscribed will be called
-		//		with a function signature like: function(a,b,c){ ... }
+		//		with a function signature like: function(arg_obj){ ... }
 		//
-		//	|		$.publish("/some/topic", ["a","b","c"]);
+		//	|		$.publish("/some/topic", {"arg_name":arg_value});
 		cache[topic] && d.each(cache[topic], function(){
-			this.apply(d, args || []);
+			//alert('pub '+topic+' with '+JSON.stringify(args));
+			this.apply(d, [args] || []);
 		});
 	};
 
@@ -47,7 +48,7 @@
 		//		A handle which can be used to unsubscribe this particular subscription.
 		//	
 		// example:
-		//	|	$.subscribe("/some/topic", function(a, b, c){ /* handle data */ });
+		//	|	$.subscribe("/some/topic", function(arg_obj){ /* handle arg_obj.data */ });
 		//
 		if(!cache[topic]){
 			cache[topic] = [];
